608eca84d20081acfd4c8a0bad1d83f2d1716fe0
Khoa Tran <jacktran1799@gmaail.com>
2020-04-08 22:31:31 -0500

    finished adding todo, will add search and toggle complete functionality

---
+++ reducer-todo/src/App.js
@@ -0,0 +1,39 @@
+import React, {useReducer, useState} from 'react';
+import logo from './logo.svg';
+import {todoList, reducer} from './reducers/reducer';
+import './App.css';
+
+function App() {
+  const [state,dispatch]= useReducer(reducer,todoList);
+  const [task,addTask]= useState('');
+  return (
+    <>
+      <div className='sticky-wrap'>
+      {state.map(each => 
+        <div className='sticky-note' key={each.id}>
+           <p> Task : {each.item}</p> 
+           {each.completed ? <p> Completed : Yes</p> : <p> Completed : No </p>}
+           
+        </div>
+      )}
+     
+      </div>
+      <div className='form'>
+        <form>
+           <h1> Add new task </h1>
+           <label> Task </label>
+          <input type='text' onChange={(e)=> addTask(e.target.value)} />
+          <i onClick={()=>dispatch({type: 'Add task', payload: task})} className="fas fa-plus-circle"> </i>
+          <br></br>
+          <h1> Search Tasks</h1>
+          <label> Search Tasks</label>
+          <input  type='text' onChange={(e)=> addTask(e.target.value)} />
+          <i onClick={()=>dispatch({type: 'Add task', payload: task})} className="fas fa-search"> </i>
+          
+        </form>
+      </div>
+    </>
+      
+  )}
+
+export default App;
---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,32 @@
+export const todoList=[
+    {
+        item: 'Master JavaScript',
+        completed: false,
+        id: 1
+    },
+    {
+        item: 'Learn React',
+        completed: false,
+        id: 2
+    },
+]
+
+
+
+export function reducer(state,action){
+    switch(action.type){
+    case 'Add task':
+     console.log(state)
+     return [
+         ...state,
+         {
+         item: action.payload,
+         completed: false,
+         id: Date.now()
+         }
+     ]
+    
+    default:
+    return 
+    }
+}
